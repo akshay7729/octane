84153187147def048fa6c5db82456c3198ee6641
akshay <akshay.khobragade7875@gmail.com>
2019-12-04 00:45:32 +0530

    Header redesign

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,16 +1,8 @@
 import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faUser, faHeart, faSearch, faHome } from '@fortawesome/free-solid-svg-icons'
-import {
-  Collapse,
-  Navbar,
-  NavbarToggler,
-  NavbarBrand,
-  Nav,
-  NavItem,
-  NavLink
-} from 'reactstrap';
+import { faUser, faHeart, faSearch, faShoppingBag } from '@fortawesome/free-solid-svg-icons'
+import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink, Input} from 'reactstrap';
 
 const Header = (props) => {
   const [isOpen, setIsOpen] = useState(false);
@@ -19,35 +11,43 @@ const Header = (props) => {
 
   return (
     <div>
-      <Navbar light expand="md" className="text-uppercase">
+      <Navbar light expand="md" className="text-uppercase pt-0">
         <NavbarToggler onClick={toggle} />
         <Collapse isOpen={isOpen} navbar>
-          <Nav className="mr-auto w-100" navbar>
+          <NavbarBrand href="/" className="text-uppercase mr-auto logo pt-2">octane tech</NavbarBrand>
+          <NavItem className="searchBox d-flex justify-content-center pr-3 mr-auto w-100 text-center">
+            <span className="searchLogo"><FontAwesomeIcon icon={faSearch} /></span>
+            <Input 
+              type="text" 
+              name="search" 
+              placeholder="SEARCH    [For eg. iPhone11, Macbook Pro 16]" 
+              className="mainSearch border-0"
+            />
+          </NavItem>
+          <Nav navbar className="flex-row-reverse">
             <NavItem>
-              <NavLink href="/"><FontAwesomeIcon icon={faHome} /> Home</NavLink>
+              <NavLink href="/" className="text-center">
+                <FontAwesomeIcon icon={faUser} />
+                <div className="nav-right-icon-desc">Akshay</div>
+              </NavLink>
             </NavItem>
             <NavItem>
-              <NavLink href="/">Phones</NavLink>
+              <NavLink href="/" className="text-center">
+                <div>
+                  <span className="icon-bubbles">5</span>
+                  <FontAwesomeIcon icon={faShoppingBag} />
+                </div>
+                <div className="nav-right-icon-desc text-uppercase">cart</div>
+              </NavLink>
             </NavItem>
             <NavItem>
-              <NavLink href="/">Macbooks</NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink href="/">Ipad</NavLink>
-            </NavItem>
-          </Nav>
-
-          <NavbarBrand href="/" className="text-uppercase mr-auto logo">octane jump</NavbarBrand>
-
-          <Nav navbar className="w-100 flex-row-reverse">
-            <NavItem>
-              <NavLink href="/"><FontAwesomeIcon icon={faUser} /></NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink href="/"><FontAwesomeIcon icon={faHeart} /></NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink href="/"><FontAwesomeIcon icon={faSearch} /></NavLink>
+              <NavLink href="/" className="text-center">
+                <div>
+                  <span className="icon-bubbles">7</span>
+                  <FontAwesomeIcon icon={faHeart} />
+                </div>
+                <div className="nav-right-icon-desc text-uppercase">wishlist</div>
+              </NavLink>
             </NavItem>
           </Nav>
         </Collapse>
